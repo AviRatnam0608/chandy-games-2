f44b1a5021ff0132bbc576a9ec5644c4555b77a3
Avinash Ratnam <avinashr@umich.edu>
2025-02-08 19:48:39 -0500

    Made letter private

--- app/letter/page.tsx
+++ app/letter/page.tsx
@@ -5,6 +5,7 @@ import LetterVerificationPopup from "@/components/letter/LetterVerificationPopup
 import { useState } from "react";
 import RejectPopup from "@/components/letter/RejectPopup";
 import YesPopup from "@/components/letter/YesPopup";
+import { LetterText } from "@/components/letter/LetterText";
 
 export default function Letter() {
   const [showPopup, setShowPopup] = useState(true);
@@ -28,21 +29,7 @@ export default function Letter() {
       {!showPopup && (
         <>
           <h1 className="text-4xl font-bold mb-8 text-black">Hi Chanz</h1>
-          <p className="text-lg mb-8 text-black w-3/4">
-            How's it going pineapple. You really thought I was not gonna ask you
-            to be my valentine huh. Well, think again 😤. <br /> I am literally
-            in love with you, so of course I was gonna ask you in the most extra
-            way possible. I had a couple of ideas for this, like sending a
-            letter (I even took sheets from office for this), send you some
-            dessert with a note, or this, a full-on clone of NYT Games, but
-            chandy themed. <br /> Now nothing actually happens when you say yes
-            (or no 😔), other than the satisfaction of knowing that in 2025, I
-            am your Valentine, and you are mine 🥰🥰🥰🥰. So till I show up next
-            weekend (Feb 14th 2025 if, for some reason, we come back to this
-            website later on), enjoy some games I made for you. Ignore any and
-            all bugs because, unlike me, nothing is perfect. <br /> I love you
-            pookie, and so that begs the question....
-          </p>
+          <LetterText />
 
           <div className="mt-4">
             <h1 className="text-2xl font-bold mb-8 text-black">
