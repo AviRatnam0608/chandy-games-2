519b6381c9ca4b0d13b449ede83667721f157132
Avinash Ratnam <avinashr@umich.edu>
2025-02-08 19:49:41 -0500

    I had to add it back lol

---
+++ components/letter/LetterText.tsx
@@ -0,0 +1,18 @@
+export const LetterText = () => {
+  return (
+    <p className="text-lg mb-8 text-black w-3/4">
+      How's it going pineapple. You really thought I was not gonna ask you to be
+      my valentine huh. Well, think again 😤. <br /> I am literally in love with
+      you, so of course I was gonna ask you in the most extra way possible. I
+      had a couple of ideas for this, like sending a letter (I even took sheets
+      from office for this), send you some dessert with a note, or this, a
+      full-on clone of NYT Games, but chandy themed. <br /> Now nothing actually
+      happens when you say yes (or no 😔), other than the satisfaction of
+      knowing that in 2025, I am your Valentine, and you are mine 🥰🥰🥰🥰. So
+      till I show up next weekend (Feb 14th 2025 if, for some reason, we come
+      back to this website later on), enjoy some games I made for you. Ignore
+      any and all bugs because, unlike me, nothing is perfect. <br /> I love you
+      pookie, and so that begs the question....
+    </p>
+  );
+};
